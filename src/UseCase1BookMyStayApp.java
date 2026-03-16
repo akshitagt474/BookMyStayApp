de8eecf1cbc4f16f1225320f299b5f816c46d6c2
Akshita Guha Thakurta <gt.akshita@gmail.com>
2026-03-16 10:11:50 +0530

    feature UC2 added

--- src/UseCase1BookMyStayApp.java
+++
@@ -1,6 +0,0 @@
-public class UseCase1BookMyStayApp {
-    public static void main(String args[]) {
-        System.out.println("Welcome to the hotel booking management system!");
-        System.out.println("System initialised successfully.");
-    }
-}
