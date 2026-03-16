a6fd259aac364c34b040effe429fc51c13788334
Akshita Guha Thakurta <sara@Akshitas-MacBook-Air-2.local>
2026-03-16 08:49:48 +0530

    Added feature UC1

---
+++ src/UseCase1BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class UseCase1BookMyStayApp {
+    public static void main(String args[]) {
+        System.out.println("Welcome to the hotel booking management system!");
+        System.out.println("System initialised successfully.");
+    }
+}
